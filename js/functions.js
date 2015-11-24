488a4bc9697190eafedbe8319c2087623e3f796f
Malith <malit.tilak@gmail.com>
2015-11-24 07:46:50 +0530

    add event listeners pase 1

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,12 @@
 /**
  * Created by Malith on 11/23/2015.
  */
+var buffer = 0;
+var statement = '';
 
-function addition(a,b){
-    alert('addition button pressed');
+function addition(){
+
+
 }
 
 function subsription(a,b){
@@ -15,8 +18,38 @@ function division(a,b){
 function multiplication(a,b){
     return a*b;
 }
-function squire(a){
-    return a*a;
+function squire(){
+    statement = statement +"&sup2;";
+    display();
 }
 
-document.getElementById("addition").addEventListener("click", addition);
+function display(){
+    document.getElementById("calDisplay").value = statement+ "\n" + buffer
+}
+
+function displaytemp(){
+    statement+=this.innerHTML;
+    display()
+}
+
+function doBackspace(){
+    statement = statement.substring(0,statement.length-1);
+    display();
+}
+
+document.getElementById("add").addEventListener("click", addition);
+
+var arr = document.getElementsByTagName('button');
+var len = arr.length;
+var i;
+for(i=0;i<len;i++) {
+    document.getElementsByTagName('button')[i].addEventListener("click", displaytemp);
+}
+
+document.getElementById("backspace").removeEventListener("click", displaytemp);
+document.getElementById("clear").removeEventListener("click", displaytemp);
+document.getElementById("sol").removeEventListener("click", displaytemp);
+document.getElementById("sqr").removeEventListener("click", displaytemp);
+
+document.getElementById("backspace").addEventListener("click", doBackspace);
+document.getElementById("sqr").addEventListener("click", squire);
